a719281fba739ab12c6424dcecd507dcb622e013
Jesus Valera <jvalera@pinnacleaerospace.com>
2022-03-28 17:53:15 -0700

    Adding basic components
    -AddTask componet
    -Button componet
    -Header componet
    -Task componet
    -Tasks componet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from 'react'
 
 function App() {
+  const[tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Doctore appointment',
+        day: 'Feb 6th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Doctora appointment',
+        day: 'Feb 7th at 2:30pm',
+        reminder: false
+    }
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((t) => t.id != id));
+  }
+
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((t) => 
+      t.id == id ? {...t, reminder:!t.reminder} : t))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="base-container">
+      <Header/>
+      <AddTask/>
+      {tasks.length > 0 ? 
+        (<Tasks tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}/>) : (<p>Not task to show</p>)}
     </div>
   );
 }
